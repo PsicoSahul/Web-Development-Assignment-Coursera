78968d220b03822a7d0d78338245eca9374d54e3
PsicoSahul <148448916+PsicoSahul@users.noreply.github.com>
2024-05-14 12:35:08 +0530

    Create script.js

---
+++ Module_4_Assignment/script.js
@@ -0,0 +1,17 @@
+// Wrap the entire contents of script.js inside of an IIFE
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+    for (var i = 0; i < names.length; i++) {
+
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+
+})();
